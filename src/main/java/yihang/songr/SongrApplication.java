42ece9f62234d2bb3a696f9fddae130756f1e0d9
Zhang <zyihang@38f9d3618f7e.ant.amazon.com>
2020-01-21 11:20:27 -0800

    initial commit from Spring init

---
+++ src/main/java/yihang/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package yihang.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yihang/songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package yihang.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
